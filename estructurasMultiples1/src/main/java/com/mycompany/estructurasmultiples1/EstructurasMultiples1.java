a5486f3d04a8831bdee84938fecc50709c9c539d
Leyder2023 <151747360+Leyder2023@users.noreply.github.com>
2024-06-14 23:51:17 -0500

    Add files via upload

---
+++ estructurasMultiples1/src/main/java/com/mycompany/estructurasmultiples1/EstructurasMultiples1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.estructurasmultiples1;
+
+/**
+ *
+ * @author john
+ */
+public class EstructurasMultiples1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
